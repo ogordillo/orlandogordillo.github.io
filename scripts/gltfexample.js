505f7b3dd630a8e80e128a63c6db0cef223d264d
Orlando Gordillo <ogordillo@miners.utep.edu>
2019-03-12 23:50:47 -0500

    gasp

--- scripts/gltfexample.js
+++ scripts/gltfexample.js
@@ -99,7 +99,42 @@ function init() {
 
 
 
+        var gaugeData = {'data': 80.0}
+  
+    // create a chart and set options
+    // note that we bind the chart to the element with id equal to chart1 via the c3.js API
+    var chart = c3.generate({
+        bindto: '#chart1',
+        data: {
+            json: gaugeData,
+            type: 'gauge',
+        },
+        gauge: {
+            label:{
+                //returning the value here and not the ratio
+                format: function(value, ratio){ return value;}
+            },
+            min: 0,
+            max: 100,
+            width: 15,
+            units: 'value' //this is only the text for the label
+        }
+    });
 
+
+        setInterval(function () {
+        
+        // create a random value between 0 and 100, rounded to 2 digits
+         var newValue = Math.floor(100 * Math.random());
+        
+        // // create a data array holding the random value
+         var newData = {'data': newValue };
+        
+        // // tell the chart to load the new data
+         chart.load({
+           json: newData
+         });
+    }, 2000);
 
     // var particleGeo3 = new THREE.Geometry();
     //     var particleMat3 = new THREE.PointsMaterial({
